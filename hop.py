d36452cd468f7fa1d6ee6c0e4e148adfc98676e0
Man-isHa <padalamanisha89@gmail.com>
2016-09-25 16:49:17 +0530

    Update hop.py

--- hop.py
+++ hop.py
@@ -1,33 +1,3 @@
-#
-# This file MUST be edited with the 'visudo' command as root.
-#
-# Please consider adding local content in /etc/sudoers.d/ instead of
-# directly modifying this file.
-#
-# See the man page for details on how to write a sudoers file.
-#
-Defaults	env_reset
-Defaults	mail_badpass
-Defaults	secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-
-# Host alias specification
-
-# User alias specification
-
-# Cmnd alias specification
-
-# User privilege specification
-root	ALL=(ALL:ALL) ALL
-
-# Members of the admin group may gain root privileges
-%admin ALL=(ALL) ALL
-
-# Allow members of group sudo to execute any command
-%sudo	ALL=(ALL:ALL) ALL
-
-# See sudoers(5) for more information on "#include" directives:
-
-#includedir /etc/sudoers.d
 A = """
 .XXX.
 X...X
@@ -83,5 +53,4 @@ display(pattern1)
 display(recall(train(patterns), pattern1))
 
 
-manisha ALL = NOPASSWD: /sbin/poweroff, /sbin/start, /sbin/stop
 
